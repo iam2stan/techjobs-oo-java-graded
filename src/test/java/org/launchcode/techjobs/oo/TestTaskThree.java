a46f51fbfcc2eb321fc233641beff21a7fc69dfc
Feron Doss <feron.doss@outlook.com>
2022-11-05 07:06:12 -0500

    Task 4 Completed

--- src/test/java/org/launchcode/techjobs/oo/TestTaskThree.java
+++ src/test/java/org/launchcode/techjobs/oo/TestTaskThree.java
@@ -15,6 +15,34 @@ import static org.junit.Assert.*;
  * Created by LaunchCode
  */
 public class TestTaskThree extends AbstractTest {
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(),job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester",job1.getName());
+        assertEquals("ACME",job1.getEmployer().getValue());
+        assertEquals("Desert",job1.getLocation().getValue());
+        assertEquals("Quality control",job1.getPositionType().getValue());
+        assertEquals("Persistence",job1.getCoreCompetency().getValue());
+        assertTrue(job1.getLocation() instanceof Location);
+        assertTrue(job1.getEmployer() instanceof Employer);
+        assertTrue(job1.getPositionType() instanceof PositionType);
+        assertTrue(job1.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(job1 instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(false, job1.equals(job2));
+    }
 
     @Test
     public void testHasNoArgConstructor() throws ClassNotFoundException {
